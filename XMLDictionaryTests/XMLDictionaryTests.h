f3c7118cc9108add079998454a993b57eeb15772
Adam Kirk <atomkirk@gmail.com>
2012-08-06 08:44:11 -0600

    wrapping in xcode project

---
+++ XMLDictionaryTests/XMLDictionaryTests.h
@@ -0,0 +1,13 @@
+//
+//  XMLDictionaryTests.h
+//  XMLDictionaryTests
+//
+//  Created by Adam Kirk on 8/6/12.
+//  Copyright (c) 2012 Mysterious Trousers. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XMLDictionaryTests : SenTestCase
+
+@end
---
+++ XMLDictionaryTests/XMLDictionaryTests.m
@@ -0,0 +1,32 @@
+//
+//  XMLDictionaryTests.m
+//  XMLDictionaryTests
+//
+//  Created by Adam Kirk on 8/6/12.
+//  Copyright (c) 2012 Mysterious Trousers. All rights reserved.
+//
+
+#import "XMLDictionaryTests.h"
+
+@implementation XMLDictionaryTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in XMLDictionaryTests");
+}
+
+@end
